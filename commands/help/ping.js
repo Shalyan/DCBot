dc8978229d130e917920aa263bb6d4b25f975eae
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-25 11:21:10 -0500

    Update ping.js

--- commands/help/ping.js
+++ commands/help/ping.js
@@ -3,6 +3,6 @@ module.exports = {
   description: "Tiempo de respuesta",
   permissions: ["ADMINISTRATOR"],
   async execute(client, message, args, discord) {
-    return message.reply("Mi ping es de "+client.ws.ping+"ms");
+    return message.reply("me actualize Mi ping es de "+client.ws.ping+"ms");
   },
 };
