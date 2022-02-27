b46c686667d3f5b267d09c9bbc4b04298bf8fe7d
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 17:17:43 -0500

    Update avatar.js

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -13,12 +13,12 @@ let embedavatar = new Discord.MessageEmbed()
 
 .setTitle(`Foto de **${usuario.user.username}**`)
 .setImage(usuario.user.displayAvatarURL({ size: 1024, dynamic: true }))
-.setFooter(`Pedido por ${message.member}`)
+.setFooter(`Pedido por ${message.user.username}`)
 .setTimestamp()
 
 if (message.channel.id = "947570709050298378") return message.reply({ embeds: [embedavatar] })
 
-if (message.chanel.id != "947570709050298378") return message.reply("Dirigete al canal de comandos para usar este comando")
+if (message.chanel.id != "947570709050298378") return message.channel.send("Dirigete al canal de comandos para usar este comando")
  }
 
 }
