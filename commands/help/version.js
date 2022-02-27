9e6f66695761228f9b1010c66047a5c571fab39d
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 12:39:53 -0500

    Update version.js

--- commands/help/version.js
+++ commands/help/version.js
@@ -7,7 +7,7 @@ module.exports = {
     //# MENSAJES
     const msgA = {
       title: "Version del bot",
-      description: "EnCubot 0.0.7",
+      description: "EnCubot 0.0.8",
       color: 10181046,
       footer: {
         text: "EnCubos Network | Discord & Minecraft",
