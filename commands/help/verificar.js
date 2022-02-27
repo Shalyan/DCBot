5b17779742f653074b2790723fc4f0370b4aea7b
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 10:22:30 -0500

    Create verificar.js

---
+++ commands/help/verificar.js
@@ -0,0 +1,33 @@
+module.exports = {
+  name: "verificar",
+  aliases: ["verify"],
+  permissions: ["ADMINISTRATOR"],
+  description: "Para verificarse",
+  async execute(client, message, args, discord) {
+    //% BUTTONS
+    const btn1 = new discord.MessageButton()
+      .setCustomId("verify")
+      .setLabel("Verificarme")
+      .setStyle("SUCCESS");
+    //% BUTTONS
+
+    //& FILA
+
+    const fila = new discord.MessageActionRow().addComponents(btn1);
+
+    //& FILA
+
+    //# MENSAJES
+
+    const msgE = {
+      title: "Verificacion | <a:emoji_7:809833434766639134>",
+      description: "Dale click al boton para que puedas ver nuestros canales y formes parte de nuestra hermosa comunidad",
+      color: 3066993,
+      },
+    };
+
+    //# MENSAJES
+
+    message.channel.send({ embeds: [msgE], components: [fila] });
+  },
+};
