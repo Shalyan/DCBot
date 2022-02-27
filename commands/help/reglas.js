e6ed12064a6d5ea0c55517d37139bb04673a8c49
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 14:45:39 -0500

    Create reglas.js

---
+++ commands/help/reglas.js
@@ -0,0 +1,35 @@
+module.exports = {
+  name: "reglas",
+  aliases: ["rules"],
+  description: "Muestra las reglas",
+  async execute(client, message, args, discord) {
+    //# MENSAJES
+    
+    const msgR = {
+      title: "Reglas",
+      description: "*Favor de leer todas nuestras reglas para evitar sanciones y pasarla bien*",
+      color: 2067276,
+      fields: [
+		      {
+			        name: 'No está permitido publicar contenido u enlaces que puedan considerarse como:',
+			        value: '- Insultos',
+              value: '- NSFW',
+              value: '- Politico',
+              value: '- Racista',
+              value: '- Religioso',
+		      },
+      author: {
+        name: "EnCubos Netwowk",
+        icon_url: "https://media.discordapp.net/attachments/945744137444016211/945747416563470366/EnCubos.PNG",
+      },
+      timestamp: new date(),
+      footer: {
+        name: "Discord & Minecraft"
+      },
+    };
+
+    //# MENSAJES
+  
+     message.channel.send({ embeds: [msgR] });
+  },
+};
