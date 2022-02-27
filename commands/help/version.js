f3a38b146cbebc75b5524cb8b363062e4db48401
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 11:16:12 -0500

    Update version.js

--- commands/help/version.js
+++ commands/help/version.js
@@ -1,8 +1,21 @@
 module.exports = {
   name: "version",
-  description: "Ver la versión del bot",
+  aliases: [],
   permissions: ["ADMINISTRATOR"],
+  description: "Para ver la version del bot",
   async execute(client, message, args, discord) {
-    return message.reply("Versión: EncuBot0.0.1");
+    //# MENSAJES
+    const msgA = {
+      title: "Version del bot",
+      description: "EnCubot 0.0.5",
+      color: 10181046,
+      footer: {
+        text: "EnCubos Network | Discord & Minecraft",
+        icon_url: "https://media.discordapp.net/attachments/945744137444016211/945747416563470366/EnCubos.PNG",
+      },
+    };
+
+
+    message.channel.send({ embeds: [msgA] });
   },
 };
