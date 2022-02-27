691ec5f51a0a71ecbc8fb4c61b1907275f4d7333
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 14:04:06 -0500

    Update clear.js

--- commands/help/clear.js
+++ commands/help/clear.js
@@ -4,6 +4,9 @@ module.exports = {
     aliases: ["del", "borrar", "limpiar"],
     description: "Borra una cierta cantidad de mensajes",
     async execute(client, message, args, discord) {
+      // Permisos
+      if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Te faltan permisos para ejecutar este comando")
+      // Permisos 
       if (!args[0]) return message.reply("Ingresa numero de mensajes a borrar");
       if (isNaN(args[0])) return message.reply("Ingresa un numero");
       if (args[0] > 100) return message.reply("Debe ser un numero menor a 100");
