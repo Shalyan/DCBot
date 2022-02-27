bea74141718d4a6dce37158b621ca49a77308979
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-27 17:53:22 -0500

    Update avatar.js

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -18,7 +18,7 @@ let embedavatar = new Discord.MessageEmbed()
 })
 .setTimestamp()
 
-if (message.channel.id != "947570709050298378") return message.reply("dirigete al canal de comandos para usar este comando")
+if (message.channel.id != "947570709050298378") return message.reply("dirigete al canal de <#947570709050298378> para usar este comando")
   
 if (message.channel.id = "947570709050298378") return message.reply({ embeds: [embedavatar] })
   
