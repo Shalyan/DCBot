eb1c313c708d28e05a1a5a05530d13d78d691127
cconfetti <oagz07@gmail.com>
2022-02-28 14:15:29 -0500

    main

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -1,28 +1,28 @@
 const Discord = require('discord.js')
 
 module.exports = {
-  name: "avatar",
-  aliases: [],
-  description: "Muestra la foto de perfil de un usuario",
-  
-async execute (client, message, args){
+    name: "avatar",
+    aliases: [],
+    description: "Muestra la foto de perfil de un usuario",
 
-let usuario = message.mentions.members.first() || message.member;
+    async execute(client, message, args) {
 
-let embedavatar = new Discord.MessageEmbed()
+        let usuario = message.mentions.members.first() || message.member;
 
-.setTitle(`Foto de **${usuario.user.username}**`)
-.setImage(usuario.user.displayAvatarURL({ size: 1024, dynamic: true }))
-.setFooter({
-  text: `Pedido por ${message.author.username}`
-})
-.setTimestamp()
+        let embedavatar = new Discord.MessageEmbed()
 
-if (message.channel.id != "947570709050298378") return message.reply("dirigete al canal de <#947570709050298378> para usar este comando")
-  
-if (message.channel.id = "947570709050298378") return message.reply({ embeds: [embedavatar] })
-  
-}
- 
+        .setTitle(`Foto de **${usuario.user.username}**`)
+            .setImage(usuario.user.displayAvatarURL({ size: 1024, dynamic: true }))
+            .setFooter({
+                text: `Pedido por ${message.author.username}`
+            })
+            .setTimestamp()
+
+        if (message.channel.id != "947570709050298378") return message.reply("dirigete al canal de <#947570709050298378> para usar este comando")
+
+        if (message.channel.id = "947570709050298378") return message.reply({ embeds: [embedavatar] })
+
+    }
+
 
 };
--- commands/general/say.js
+++ commands/general/say.js
@@ -1,16 +1,16 @@
 const { MessageEmbed } = require("discord.js");
 
-module.exports= {
+module.exports = {
     name: "say",
     description: "El bot repetirá la imagen que subas o el texto que escribas",
-    async execute(client, message, args , discord) {
+    async execute(client, message, args, discord) {
         let sayimage = message.attachments.first()
         const saytext = args.join(" ")
-        if(!saytext && !sayimage) return message.channel.send('Debes escribir algo o enviar una imagen!')
+        if (!saytext && !sayimage) return message.reply('Debes escribir algo o enviar una imagen!')
 
         const sayembed = new MessageEmbed()
-        .setDescription(saytext)
-        if(sayimage){sayembed.setImage(sayimage.url)}
+            .setDescription(saytext)
+        if (sayimage) { sayembed.setImage(sayimage.url) }
 
         message.channel.send({ embeds: [sayembed] })
 
