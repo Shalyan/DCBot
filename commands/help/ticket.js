ac7afc9b0ea5fb460ada3137a20de383c6ba7c45
cconfetti <100319090+cconfetti@users.noreply.github.com>
2022-02-28 15:20:55 -0500

    Create ticket.js

---
+++ commands/help/ticket.js
@@ -0,0 +1,39 @@
+module.exports = {
+    name: "ticket",
+    aliases: [""],
+    permissions: ["ADMINISTRATOR"],
+    description: "Para abrir ticket",
+    async execute(client, message, args, discord) {
+        // Permisos
+        if (!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Te faltan permisos para ejecutar este comando")
+            // Permisos
+            //% BUTTONS
+        const btn1 = new discord.MessageButton()
+            .setCustomId("ticket")
+            .setLabel("Abrir")
+            .setEmoji("🎴")
+            .setStyle("SUCCESS");
+        //% BUTTONS
+
+        //& FILA
+
+        const fila = new discord.MessageActionRow().addComponents(btn1);
+
+        //& FILA
+
+        //# MENSAJES
+
+        const msgB = {
+            title: "🛃 | Soporte",
+            description: "Abre un ticket para que puedas hablar con nuestro equipo de soporte y asi aclarar alguna duda o ayudarte en algo.",
+            color: 3066993,
+            footer: {
+                text: "EnCubos Network | Discord & Minecraft",
+                icon_url: "https://media.discordapp.net/attachments/945744137444016211/945747416563470366/EnCubos.PNG",
+            },
+        };
+
+
+        message.channel.send({ embeds: [msgE], components: [fila] });
+    },
+};
