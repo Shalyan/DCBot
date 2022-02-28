d521d92cf90886ac67430be5f1e26c32ba9379b8
cconfetti <oagz07@gmail.com>
2022-02-28 13:58:31 -0500

    Update verificar.js

--- commands/help/verificar.js
+++ commands/help/verificar.js
@@ -1,39 +1,39 @@
 module.exports = {
-  name: "verificar",
-  aliases: ["verify"],
-  permissions: ["ADMINISTRATOR"],
-  description: "Para verificarse",
-  async execute(client, message, args, discord) {
-     // Permisos
-    if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Te faltan permisos para ejecutar este comando")
-    // Permisos
-    //% BUTTONS
-    const btn1 = new discord.MessageButton()
-      .setCustomId("acp")
-      .setLabel("Verificarme")
-      .setEmoji("<a:chec1:945073442808557699>")
-      .setStyle("SUCCESS");
-    //% BUTTONS
+    name: "verificar",
+    aliases: ["verify"],
+    permissions: ["ADMINISTRATOR"],
+    description: "Para verificarse",
+    async execute(client, message, args, discord) {
+        // Permisos
+        if (!message.member.permissions.has("ADMINISTRATOR")) return message.reply("Te faltan permisos para ejecutar este comando")
+            // Permisos
+            //% BUTTONS
+        const btn1 = new discord.MessageButton()
+            .setCustomId("acp")
+            .setLabel("Verificarme")
+            .setEmoji("<a:chec1:945073442808557699>")
+            .setStyle("SUCCESS");
+        //% BUTTONS
 
-    //& FILA
+        //& FILA
 
-    const fila = new discord.MessageActionRow().addComponents(btn1);
+        const fila = new discord.MessageActionRow().addComponents(btn1);
 
-    //& FILA
+        //& FILA
 
-    //# MENSAJES
+        //# MENSAJES
 
-    const msgE = {
-      title: "<a:emoji_7:809833434766639134> | Verificacion",
-      description: "Dale click al boton para que puedas ver nuestros canales y formes parte de nuestra hermosa comunidad",
-      color: 3066993,
-      footer: {
-        text: "EnCubos Network | Discord & Minecraft",
-        icon_url: "https://media.discordapp.net/attachments/945744137444016211/945747416563470366/EnCubos.PNG",
-      },
-    };
+        const msgE = {
+            title: "<a:emoji_7:809833434766639134> | Verificacion",
+            description: "Dale click al boton para que puedas ver nuestros canales y formes parte de nuestra hermosa comunidad",
+            color: 3066993,
+            footer: {
+                text: "EnCubos Network | Discord & Minecraft",
+                icon_url: "https://media.discordapp.net/attachments/945744137444016211/945747416563470366/EnCubos.PNG",
+            },
+        };
 
 
-    message.channel.send({ embeds: [msgE], components: [fila] });
-  },
+        message.channel.send({ embeds: [msgE], components: [fila] });
+    },
 };
